6165ef76d8fde110179b94070dd10cddc1642e2b
Carlos Conley <carlosconley@icloud.com>
2024-03-20 01:37:47 -0500

    Format

--- lua/exojay/packer.lua
+++ lua/exojay/packer.lua
@@ -4,52 +4,51 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
     -- Navigation plugins
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.6',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
     use('tpope/vim-vinegar')
 
-	-- Treesitter plugins
-	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-	use('nvim-treesitter/playground')
+    -- Treesitter plugins
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
 
-	-- Color schemes
-	use {'rose-pine/neovim', as = "rose-pine"}
-	use {'nordtheme/vim', as = "nord"}
-	use {'ellisonleao/gruvbox.nvim'}
+    -- Color schemes
+    use { 'rose-pine/neovim', as = "rose-pine" }
+    use { 'nordtheme/vim', as = "nord" }
+    use { 'ellisonleao/gruvbox.nvim' }
 
-	--Undo tree
-	use('mbbill/undotree')
+    --Undo tree
+    use('mbbill/undotree')
 
     -- Git stuff
-	use('tpope/vim-fugitive')
+    use('tpope/vim-fugitive')
     use('airblade/vim-gitgutter')
 
     -- Surround stuff
-	use('tpope/vim-surround')
+    use('tpope/vim-surround')
 
-	-- LSP
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		requires = {
-			--- Uncomment the two plugins below if you want to manage the language servers from neovim
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
+    -- LSP
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment the two plugins below if you want to manage the language servers from neovim
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'L3MON4D3/LuaSnip'},
-		}
-	}
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'L3MON4D3/LuaSnip' },
+        }
+    }
 end)
-
